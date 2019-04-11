82b6a61860c1622a1cccc89604252e1596940a37
Juliano Krieger <jkrieger@xebialabs.com>
2019-04-11 16:31:37 +0200

    LOVE-782 Added variable and create branch step

--- Releasefile.groovy
+++ Releasefile.groovy
@@ -1,15 +1,20 @@
-// Exported from:        http://juliano-mbp.local:5516/#/templates/Folderc8ddd97b0de74d65b288e746d1bbbbb2-Releasedd48169436124943951a1e818ef66c0a/releasefile
+// Exported from:        https://xl-release.xebialabs.com/#/templates/Folderdbce503315164ec3848b3058fcb37524-Releasea6867c97994c4ddc9671845ef0ed0de4/releasefile
 // XL Release version:   8.6.1
-// Date created:         Tue Apr 09 09:30:00 CEST 2019
+// Date created:         Thu Apr 11 16:29:34 CEST 2019
 
 xlr {
   template('Release Blueprints') {
-    folder('Blueprints')
+    folder('Shared')
+    variables {
+      stringVariable('BLUEPRINTS_VERSION') {
+        label 'Blueprints Version'
+      }
+    }
     scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2019-04-08T09:00:00+0200')
     phases {
-      phase('New Phase') {
+      phase('Verification') {
         tasks {
-          script('Check Travis CI build') {
+          script('Verify Travis CI last build') {
             script (['''\
 import urllib2
 import json
@@ -25,7 +30,6 @@ def make_req(url, body=None, token=None):
   return json.loads(response.read())
 
 repo = make_req(travis_url)['repo']
-print repo
 print("\\nLast build #: " + repo['last_build_number'])
 print("\\nLast build status: " + repo['last_build_state'])
 if repo['last_build_state'] != "passed":
@@ -34,6 +38,35 @@ if repo['last_build_state'] != "passed":
 '''])
           }
         }
+      }
+      phase('Github') {
+        color '#0099CC'
+        tasks {
+          custom('Create Branch') {
+            script {
+              type 'github.CreateBranch'
+              server 'XebiaLabs GitHub (from community GitHub plugin)'
+              organization 'xebialabs'
+              repositoryName 'blueprints'
+              newBranch '${BLUEPRINTS_VERSION}-maintenance'
+            }
+          }
+          custom('Create PR development -> master') {
+            script {
+              type 'github.CreatePullRequest'
+              server 'XebiaLabs GitHub (from community GitHub plugin)'
+            }
+          }
+          custom('Merge PR development -> master') {
+            script {
+              type 'github.MergePullRequest'
+              server 'XebiaLabs GitHub (from community GitHub plugin)'
+            }
+          }
+        }
+      }
+      phase('Dist') {
+        color '#0099CC'
       }
     }
     
